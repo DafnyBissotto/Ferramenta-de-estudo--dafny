daa25f5f25fd199332619a4edc19173a66d53908
DafnyBissotto <dafny.bissotto@escola.pr.gov.br>
2025-09-10 13:36:57 +0000

    criamos os JS aula 7

--- perguntas.js
+++ perguntas.js
@@ -1,53 +1,53 @@
 criaCartao(
     'programação',
     'o que é HTML',
-    resposta
+    'é uma linguagem de Programação que nos permite criar a estrutura do nosso site'
 )
 
 criaCartao(
     'programação',
     'o que é CSS',
-    resposta
+'É uma linguagem de programação'
 )
 
 criaCartao(
     'programação',
     'o que é o java script ',
+    'o java script(js) é uma linguagem de programação que nos permite interagir com a pagina',
+)
+
+criaCartao(
+    'programação',
+    'o qual a diferença emtra a teg title que usamos na teg heard e a teg h1 que usamos na teg body',
     resposta
 )
 
 criaCartao(
     'programação',
-    'o qual a diferença',
+    'qual a diferença emtre as teg head e header',
+    'head é a teg de configuração, tudo que fizemos dentro dela, não aparesera na pagina do nosso site'
+)
+
+criaCartao(
+    'programação',
+    'pra que serve o : root no CSS',
     resposta
 )
 
 criaCartao(
     'programação',
-    pergunta,
-    resposta
+    'qual a diferemça emtre magin e padding',
+    'margin é epaçamento externo enquanto o padding é espaçamento interno',
 )
 
 criaCartao(
     'programação',
-    pergunta,
-    resposta
+    'o que é a teg footer',
+    'é uma teg utilyzada para criar o roda pé'
 )
 
 criaCartao(
     'programação',
-    pergunta,
-    resposta
-)
-
-criaCartao(
-    'programação',
-    pergunta,
-    resposta
-)
-
-criaCartao(
-    'programação',
-    pergunta,
-    resposta
+    'para que serve a teg article',
+    'para criar um comteudo ou elemento idepedente. No nosso caso estamos usando para criar os cartoes de estudo'
 )
