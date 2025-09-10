e99940197908ee08b24643c1aeecae247f4ee166
DafnyBissotto <dafny.bissotto@escola.pr.gov.br>
2025-09-10 12:30:47 +0000

    fizemos 9 cartoes JS

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'programação',
+    'o que é HTML',
+    resposta
+)
+
+criaCartao(
+    'programação',
+    'o que é CSS',
+    resposta
+)
+
+criaCartao(
+    'programação',
+    'o que é o java script ',
+    resposta
+)
+
+criaCartao(
+    'programação',
+    'o qual a diferença',
+    resposta
+)
+
+criaCartao(
+    'programação',
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    'programação',
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    'programação',
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    'programação',
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    'programação',
+    pergunta,
+    resposta
+)
